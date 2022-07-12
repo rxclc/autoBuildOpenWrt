13edadd7ffcabdf9f9bb83e76c7729ce7507de33
rxclc <80808243+rxclc@users.noreply.github.com>
2022-07-12 19:18:20 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,19 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.253/g' package/base-files/files/bin/config_generate
+
+# luci-app-passwall
+git clone -b luci --depth 1 https://github.com/xiaorouji/openwrt-passwall.git package/passwall/luci-app-passwall/
+git clone -b packages --depth 1 https://github.com/xiaorouji/openwrt-passwall.git package/passwall/
+
+# luci-app-ssrp
+git clone -b master --depth 1 https://github.com/fw876/helloworld.git package/helloworld/
+
+# luci-theme-argon
+rm -rf feeds/luci/themes/luci-theme-argon/
+git clone -b 18.06 --depth 1 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon/
+
+# luci-app-vssr
+#git clone -b master --depth 1 https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb/
+#git clone -b master --depth 1 https://github.com/jerrykuku/luci-app-vssr.git package/lean/luci-app-vssr/
