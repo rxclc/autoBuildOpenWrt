1f101c38c8f8fb4f60b08fb791117c4f0e908bf9
rxclc <80808243+rxclc@users.noreply.github.com>
2022-07-12 22:23:58 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,15 +14,15 @@
 sed -i 's/192.168.1.1/192.168.1.253/g' package/base-files/files/bin/config_generate
 
 # luci-app-passwall
-git clone -b luci --depth 1 https://github.com/xiaorouji/openwrt-passwall.git package/passwall/luci-app-passwall/
-git clone -b packages --depth 1 https://github.com/xiaorouji/openwrt-passwall.git package/passwall/
+#git clone -b luci --depth 1 https://github.com/xiaorouji/openwrt-passwall.git package/passwall/luci-app-passwall/
+#git clone -b packages --depth 1 https://github.com/xiaorouji/openwrt-passwall.git package/passwall/
 
 # luci-app-ssrp
 git clone -b master --depth 1 https://github.com/fw876/helloworld.git package/helloworld/
 
 # luci-theme-argon
-rm -rf feeds/luci/themes/luci-theme-argon/
-git clone -b 18.06 --depth 1 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon/
+#rm -rf feeds/luci/themes/luci-theme-argon/
+#git clone -b 18.06 --depth 1 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon/
 
 # luci-app-vssr
 #git clone -b master --depth 1 https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb/
