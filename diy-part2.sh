e1bd1989312a5a9f056f430a2e7ed4d6e554e193
rxclc <80808243+rxclc@users.noreply.github.com>
2022-07-16 10:44:58 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,4 +14,5 @@
 sed -i 's/192.168.1.1/192.168.1.253/g' package/base-files/files/bin/config_generate
 
 # luci-theme-argon
-git clone -b master --depth 1 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon/
+rm -rf feeds/luci/themes/luci-theme-argon/
+git clone -b 18.06 --depth 1 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon/
